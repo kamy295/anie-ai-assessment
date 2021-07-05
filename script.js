7c95e74bb63e07c018e98600f31c54d16d09b852
Kamlesh Kamble <kamlesh.kamble2.95@gmail.com>
2021-07-05 17:32:58 +0530

    ANIE-AI

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//geting the elements by ID;
 let students = document.getElementById("students");
 let lesson = document.getElementById("lesson");
 let settings = document.getElementById("settings");
 
+
+//adding event listner to handle click event;
 students.addEventListener('click', ()=> alert("You have clicked Students"));
-lesson.addEventListener('click', ()=> alert("You have clicked Lesson"));
+lesson.addEventListener('click', ()=> alert("You have clicked Lesson Plan"));
 settings.addEventListener('click', ()=> alert("You have clicked Settings"));

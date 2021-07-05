c295d30bb346f79348a6688888de9a4f72ca58e1
Kamlesh Kamble <kamlesh.kamble2.95@gmail.com>
2021-07-05 17:04:34 +0530

    ANIE-AI

---
+++ script.js
@@ -0,0 +1,7 @@
+let students = document.getElementById("students");
+let lesson = document.getElementById("lesson");
+let settings = document.getElementById("settings");
+
+students.addEventListener('click', ()=> alert("You have clicked Students"));
+lesson.addEventListener('click', ()=> alert("You have clicked Lesson"));
+settings.addEventListener('click', ()=> alert("You have clicked Settings"));
